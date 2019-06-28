4c19eff6f39e979ec775066f98df0220a4d5856a
Fernando <>
2019-06-28 09:34:22 +0700

    Approval

---
+++ app/Http/Controllers/ApprovalController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\SendMail;
+use App\Leave;
+
+class ApprovalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $leaves = Leave::where('user_id', '=', Auth::user()->id)->paginate(5);
+        return view('approval.approval', compact('leaves'))
+                    ->with('i',(request()->input('page',1) -1) *5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $leave = Leave::find($id);
+        return view('leave.edit', compact('leave'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
---
+++ resources/views/approval/approval.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <h3>Waiting For Approve</h3>
+            </div>  
+        </div>
+
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{$message}}</p>
+            </div>
+        @endif
+
+    
+        <table class="table table-hover table-sm" >
+            <tr>
+                <th width="50px"style="text-align:center;"><b>No.</b></th>
+                <th style="text-align:center;width:200px;">Name</th>
+                <th style="text-align:center;width:200px;">From</th>
+                <th style="text-align:center;width:200px;">To</th>
+                <th style="text-align:center;width:200px;">Duration</th>
+                <th style="text-align:center; width:100px;">Reason</th>
+            </tr>
+
+            @foreach ($leaves as $leave)
+                <tr>
+                    <td style="text-align:center;"><b>{{++$i}}.</b></td>
+                    <td style="text-align:center;">{{$leave->user_id}}</td>
+                    <td style="text-align:center;">{{$leave->from}}</td>
+                    <td style="text-align:center;">{{$leave->to}}</td>
+                    <td style="text-align:center;">{{$leave->duration}} days</td>
+                    <td style="text-align:center;">{{$leave->reason}}</td>
+                    <td>
+                        <form style="width:180px;">
+                            <a class="btn btn-sm btn-success" value="send" href="{{route('approval.edit',$leave->id)}}">Approve</a>
+                            <a class="btn btn-sm btn-danger" value="send" href="{{route('approval.edit',$leave->id)}}">Reject</a>
+                        </form>
+                    </td>
+                </tr>
+                
+            @endforeach
+
+        </table>
+    </div>
+@endsection
