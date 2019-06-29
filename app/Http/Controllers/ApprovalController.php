0ce7c9c57a0bc29cdde99e8428717902bed02a71
andreasbudi <48094614+andreasbudi@users.noreply.github.com>
2019-06-29 07:03:23 +0700

    nyatuin approval

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Auth;
+use App\Mail\SendMail;
 use App\Leave;
 
 class ApprovalController extends Controller
@@ -13,8 +17,91 @@ class ApprovalController extends Controller
      */
     public function index()
     {
-        $leaves = Leave::latest()->paginate(5);
+        $leaves = Leave::where('user_id', '=', Auth::user()->id)->paginate(5);
         return view('approval.approval', compact('leaves'))
                     ->with('i',(request()->input('page',1) -1) *5);
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $leave = Leave::find($id);
+        return view('approval.approvalacc', compact('leave'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $leave = Leave::find($id);
+        return view('approval.approvalrej', compact('leave'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'status' => 'required'
+        ]);
+
+        $leave = Leave::find($id);
+        $leave->status = $request->get('status');
+
+        if($leave->save()){
+            return redirect()->route('approval.index')
+                        ->with('success', 'Leave Approved');
+
+        }else{
+            return redirect()->route('approval.index')
+                        ->with('error', 'Failed approve. Please try again');
+        }      
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
 }
