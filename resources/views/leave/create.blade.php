249079530d0257af5502f7c62675ba9c2d2eeeba
andreasbudi <48094614+andreasbudi@users.noreply.github.com>
2019-07-02 15:03:00 +0700

    masih belom bisa auto add dates

--- resources/views/leave/create.blade.php
+++ resources/views/leave/create.blade.php
@@ -39,31 +39,58 @@
                                         @csrf
                                         <div class="col-md-12">
                                             <strong>From :</strong>
-                                            <input type="date" name="from" class="form-control">
+                                            <input type="date" name="from" id="from" class="form-control">
                                         </div>
                                         <br>
                                         <div class="col-md-12">
                                             <strong>Duration :</strong>
-                                            <select name="duration" id="duration" class="form-control">
+                                            <input type="text" name="duration" id="duration" class="form-control">
+                                            {{-- <select name="duration" id="duration" class="form-control" onchange="run(this.value)">
                                             <script>
+                                            function run(val) {
+                                                var formDuration = document.getElementById("duration");
+                                                var getDuration = formDuration.options[formDuration.selectedIndex].value;
+                                                // alert(getDuration);
+
+                                                document.getElementById("from").addEventListener("change", function() {
+                                                var input = new Date(this.value);
+                                                var newdate = new Date(input);
+                                                var temp = newdate.getDate();
+                                                var calculate = temp + getDuration;
+                                                newdate.setDate(calculate);
+                                                var dd = newdate.getDate();
+                                                var mm = newdate.getMonth() + 1;
+                                                var yyyy =  newdate.getFullYear();
+                                                if (dd < 10) {
+                                                dd = '0' + dd;
+                                                } 
+                                                if (mm < 10) {
+                                                mm = '0' + mm;
+                                                } 
+                                                var someFormattedDate = mm + '/' + dd + '/' + yyyy;
+                                                document.getElementById('to').value = someFormattedDate;
+                                                });
+                                            }
                                             (function() { // don't leak
                                                 var elm = document.getElementById('duration'), // get the select
                                                     df = document.createDocumentFragment(); // create a document fragment to hold the options while we create them
-                                                for (var i = 1; i <= 12; i++) { // loop, i like 42.
+                                                for (var i = 1; i <= 12; i++) { 
                                                     var option = document.createElement('option'); // create the option element
                                                     option.value = i; // set the value property
                                                     option.appendChild(document.createTextNode(i + " days")); // set the textContent in a safe way.
                                                     df.appendChild(option); // append the option to the document fragment
                                                 }
                                                 elm.appendChild(df); // append the document fragment to the DOM. this is the better way rather than setting innerHTML a bunch of times (or even once with a long string)
-                                            }());    
+                                         
+                                            }()); 
                                             </script>
-                                            </select>
+                                            </select> --}}
                                         </div>
                                         <br>
                                         <div class="col-md-12">
                                             <strong>To :</strong>
-                                            <input type="date" name="to" class="form-control">
+                                            <input type="date" name="to" id="to" class="form-control">
+                                            {{-- <input type="text" name="to" id="to" class="form-control"> --}}
                                         </div>
                                         <br>
                                         <div class="col-md-12">
