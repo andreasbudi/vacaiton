5466f4cfb11262ede8f7973adf4f52120f48888f
Fernando <>
2019-07-03 15:09:49 +0700

    edit, delete

--- resources/views/employee/profile.blade.php
+++ resources/views/employee/profile.blade.php
@@ -14,6 +14,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="m-portlet__body">
                 <div class="tab-content">
                     <div class="m-widget2">
@@ -29,7 +30,6 @@
                                     <input type="text" class="form-control " value="{{{ (Auth::user()->email) }}}" disabled>
                             </div>
                             
-                    
                             <div class="col-md-12">
                                     <strong>Department :</strong>
                                     <input type="text" class="form-control " value="{{{ (Auth::user()->department) }}}" disabled>
@@ -47,14 +47,20 @@
                                     <input type="text" class="form-control " value="{{Auth::user()->roles()->first()->name_role}}" disabled>     
                             </div>
 
-                            @if (Auth::user()->role_id != '3' && Auth::user()->role_id != '4')
+                            @if (Auth::user()->role_id == '1')
                             <div class="col-md-12">
                                     <strong>Supervisor :</strong>
                                     <input type="text" class="form-control " value="{{{ (Auth::user()->supervisors()->first()->name_supervisor) }}}" disabled>
                             </div>
                             @endif
-                            
                         </form>
+
+                        <form action="{{route('employee.edit',Auth::user())}}" style="width:180px;">
+                            @csrf
+                            @method('PUT')
+                            <button type="submit" class="btn btn-sm btn-warning">Edit</button>
+                        </form>
+
                     </div>
                 </div>
             </div>
