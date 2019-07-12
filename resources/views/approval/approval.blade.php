bf257638571a07412fde7223bbff5baf2db146d2
Fernando <>
2019-07-12 13:59:19 +0700

    Update approval.blade.php

--- resources/views/approval/approval.blade.php
+++ resources/views/approval/approval.blade.php
@@ -6,6 +6,9 @@
             <div class="row">
                     
                     <div class="col-lg-9">
+                    <h3 class="m-portlet__head-text">
+                         Hi, {{(Auth::user()->name)}}
+                    </h3>
                         <!--begin::Portlet-->
                         <div class="m-portlet" id="m_portlet">
                             <div class="m-portlet__head">
@@ -99,9 +102,9 @@
                                 </div>
                             </div>
                             <div class="m-portlet__body">
-                                {{-- <div id="m_calendar"></div> --}}
-                                {{-- {!! $calendar->calendar() !!}
-                                {!! $calendar->script() !!} --}}
+                            
+                                <div id="m_calendar"></div>
+                                 
                             </div>
                         </div>
                         <!--end::Portlet-->
@@ -146,7 +149,6 @@
                         <div class="m-portlet__head-caption">
                             <div class="m-portlet__head-title">
                                 <h3 class="m-portlet__head-text">
-                                     Hi, {{(Auth::user()->name)}}<br>
                                      Please Approve Your Team Leave Request
                                 </h3>
                             </div>
@@ -265,3 +267,175 @@
 
 
 @endsection
+
+
+@push('scripts')
+
+<script>                     
+    var CalendarExternalEvents = function() {
+        var t = function() {
+                $("#m_calendar_external_events .fc-event").each(function() {
+                    $(this).data("event", {
+                        title: $.trim($(this).text()),
+                        stick: !0,
+                        className: $(this).data("color"),
+                        description: "Lorem ipsum dolor eius mod tempor labore"
+                    }), $(this).draggable({
+                        zIndex: 999,
+                        revert: !0,
+                        revertDuration: 0
+                    })
+                })
+            },
+            e = function() {
+                var t = moment().startOf("day"),
+                    e = t.format("YYYY-MM"),
+                    i = t.clone().subtract(1, "day").format("YYYY-MM-DD"),
+                    r = t.format("YYYY-MM-DD"),
+                    n = t.clone().add(1, "day").format("YYYY-MM-DD");
+                $("#m_calendar").fullCalendar({
+                    header: {
+                        left: "prev,next today",
+                        center: "title",
+                        right: "month,agendaWeek,agendaDay,listWeek"
+                    },
+                    // defaultView: "listWeek",
+                    eventLimit: !0,
+                    navLinks: !0,
+                    events: [
+                        {
+                    title: "New Year's Day",
+                    start:  "2019-01-01",
+                    description: "New Year's Day",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Chinese New Year",
+                    start:  "2019-02-05",
+                    description: "Chinese New Year",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Bali Hindu New Year",
+                    start:  "2019-03-07",
+                    description: "Bali Hindu New Year",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Isra Mi'raj",
+                    start:  "2019-04-03",
+                    description: "Isra Mi'raj",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Good Friday",
+                    start:  "2019-04-19",
+                    description: "Good Friday",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Labour Day",
+                    start:  "2019-05-01",
+                    description: "Labour Day",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Waisak Day",
+                    start:  "2019-05-19",
+                    description: "Waisak Day",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Ascension Day of Jesus Christ",
+                    start:  "2019-05-30",
+                    description: "Ascension Day of Jesus Christ",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Pancasila Day",
+                    start:  "2019-06-01",
+                    description: "Pancasila Day",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Lebaran Holiday",
+                    start:  "2019-06-03",
+                    end: "2019-06-04",
+                    description: "Lebaran Holiday",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Hari Raya Idul Fitri",
+                    start:  "2019-06-05",
+                    end: "2019-06-06",
+                    description: "Hari Raya Idul Fitri",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Lebaran Holiday",
+                    start:  "2019-06-07",
+                    description: "Lebaran Holiday",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Idul Adha",
+                    start:  "2019-08-11",
+                    description: "Idul Adha",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Independence Day",
+                    start:  "2019-08-17",
+                    description: "Independence Day",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Islamic New Year",
+                    start:  "2019-09-01",
+                    description: "Islamic New Year",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Prophet Muhammad's Birthday",
+                    start:  "2019-11-09",
+                    description: "Prophet Muhammad's Birthday",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Christmas Holiday",
+                    start:  "2019-12-24",
+                    description: "Christmas Holiday",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                },{
+                    title: "Christmas Day",
+                    start:  "2019-12-25",
+                    description: "Christmas Day",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                }
+
+                // from DB    
+
+                @foreach($leaves as $leave)
+                    ,{
+                    title: "{{$leave->users->name}}",
+                    start:  "{{$leave->from}}",
+                    end: "{{$leave->to}}",
+                    description: "{{$leave->reason}}",
+                    className: "m-fc-event--danger m-fc-event--solid-warning"
+                    }
+                @endforeach
+
+                    ],
+                    editable: !0,
+                    droppable: !0,
+                    drop: function(t, e, i, r) {
+                        var n = $.fullCalendar.moment(t.format());
+                        n.stripTime(), n.time("08:00:00");
+                        var a = $.fullCalendar.moment(t.format());
+                        a.stripTime(), a.time("12:00:00"), $(this).data("event").start = n, $(this).data("event").end = a, $("#m_calendar_external_events_remove").is(":checked") && $(this).remove()
+                    },
+                    eventRender: function(t, e) {
+                        e.hasClass("fc-day-grid-event") ? (e.data("content", t.description), e.data("placement", "top"), mApp.initPopover(e)) : e.hasClass("fc-time-grid-event") ? e.find(".fc-title").append('<div class="fc-description">' + t.description + "</div>") : 0 !== e.find(".fc-list-item-title").lenght && e.find(".fc-list-item-title").append('<div class="fc-description">' + t.description + "</div>")
+                    }
+                })
+            };
+        return {
+            init: function() {
+                t(), e()
+            }
+        }
+    }();
+    jQuery(document).ready(function() {
+        CalendarExternalEvents.init()
+    });
+
+
+
+            </script>
+
+
+@endpush
