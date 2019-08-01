4bb89fcbe97fb08eb39aa4410e7239709b8c1edb
Fernando <>
2019-08-01 17:06:03 +0700

    Ganti wording email add member sama aktivasi akun

--- resources/views/email_addEmployee.blade.php
+++ resources/views/email_addEmployee.blade.php
@@ -1,10 +1,7 @@
-<p>Dear {{ $data['name']}},</p>
-<p>It is my pleasure to welcome you to the {{ $data['department']}} department at Difinite Company. You have <b>{{ $data['leaves_available']}} leaves available</b> within a year, please use it wisely :)</p>
-<p>This is your email <b>{{ $data['email']}}</b> and password <b>{{ $data['password']}}.</b></p>
-<p>Please change your password immediately.</p>
-<p>We’re looking forward to working with you.</p>
+<p>Hi {{ $data['name']}},</p>
+<p>Welcome onboard to our team!</p>
+<p>Here's your access to our people portal.</p> 
+<p>please <a class="btn btn-sm btn-success" value="send" href="http://localhost:8000/password/reset">click here</a> to activate your account</p>
 <br>
 <p>Regards,</p>
 <p>Admin</p>
-<a class="btn btn-sm btn-success" value="send" href="http://localhost:8000/">http://localhost:8000/</a>
-
